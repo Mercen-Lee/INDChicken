c36fbe9b6dffeb08aadb548d997d44b8f1dc41a8
이석호 <mercen@mercen.net>
2022-07-10 20:14:35 +0900

    Added Alert

--- INDChicken/ViewController.swift
+++ INDChicken/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     let photo:[String] = ["Fried", "Hot", "Soysause", "Fries", "Cheeseball"]
     let name:[String] = ["후라이드 치킨", "양념 치킨", "간장 치킨", "감자 튀김", "치즈볼"]
     let price:[Int] = [16000, 17000, 17000, 4000, 3000]
+    public var shop:[Int] = [0, 0, 0, 0, 0]
     override func viewDidLoad() {
         super.viewDidLoad()
         MenuTable.delegate = self
@@ -39,5 +40,19 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         cell.Photo.image = UIImage(named: photo[indexPath.row])
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "추가 완료", message: "장바구니로 이동하시겠습니까?", preferredStyle: UIAlertController.Style.alert)
+        let closeAction = UIAlertAction(title: "닫기", style: .default, handler: nil)
+        let okAction = UIAlertAction(title: "이동", style: .default) { (action) -> Void in
+            let viewControllerYouWantToPresent = self.storyboard?.instantiateViewController(withIdentifier: "Shop")
+            self.present(viewControllerYouWantToPresent!, animated: true, completion: nil)
+        }
+        alert.addAction(closeAction)
+        alert.addAction(okAction)
+        present(alert, animated: false, completion: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
+        shop[indexPath.row] += 1
     }
 }
